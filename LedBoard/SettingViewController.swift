7ac967b56623bdccb3ee4b6daa9e8dbd6b4425d8
qdcv0272 <qdcv0272@naver.com>
2022-03-16 21:38:05 +0900

    first commit

---
+++ LedBoard/SettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingViewController.swift
+//  LedBoard
+//
+//  Created by changhun kim on 2022/03/16.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
